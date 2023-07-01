a4b0d2492f0b7c6036055924bc6ebf40ee2af6cf
gnlda <dunlock67@gmail.com>
2023-07-01 09:07:41 +0000

    Jul 1, 2023, 12:07 PM

--- app/js/converter.js
+++ app/js/converter.js
@@ -7,13 +7,14 @@ let row2 = document.querySelector("#row2");
 let input1 = row1.querySelector("input");
 let input2 = row2.querySelector("input");
 let converterButton = document.querySelector(".converter__button");
+let change = false;
 
 
 const buyButtonClickHandler = () => {
     if (!currentState) {
         currentState = true;
         rate = 0.0000345245;
-        input1.value = 4;
+        input1.value = 4.000.toFixed(3);
         input2.value = input1.value * rate;
         sellButton.classList.toggle("active");
         buyButton.classList.toggle("active");
@@ -43,9 +44,18 @@ const sellButtonClickHandler = () => {
 
 const valueInputHandler = () => {
     input2.value = input1.value * rate;
+    change = false;
+}
+
+const firstInputClick = () => {
+    if (!change) {
+        input1.value = '';
+        change = true;
+    }
 }
 
 
 buyButton.addEventListener("click", buyButtonClickHandler);
 sellButton.addEventListener("click", sellButtonClickHandler);
-input1.addEventListener("input", valueInputHandler)
+input1.addEventListener("input", valueInputHandler);
+input1.addEventListener("click", firstInputClick);
