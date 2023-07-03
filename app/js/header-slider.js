95f781e8e10ec29aa4468dee385844fc91c0590b
revus05 <skninni05@gmail.com>
2023-07-03 15:19:13 +0000

    Jul 3, 2023, 6:19 PM

--- app/js/header-slider.js
+++ app/js/header-slider.js
@@ -64,7 +64,7 @@ const mousemoveHandler = (e) => {
 const touchmoveHandler = (e) => {
     if(isTouch) {e.preventDefault();
         delta = e.touches[0].clientX - previousOffset;
-        console.log(`delta = ${delta}`);
+        console.log(`delta = ${delta}, clientX = ${e.touches[0].clientX}`);
         let left = parseInt(slider.style.left) + delta;
         if (left < -offset && left > -sliderLength - offset) {
             slider.style.left = `${left}px`;
