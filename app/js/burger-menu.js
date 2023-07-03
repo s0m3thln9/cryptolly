8b50ebd36b54c734700be1e42923c1b01436762f
revus05 <skninni05@gmail.com>
2023-07-03 16:47:37 +0000

    Jul 3, 2023, 7:47 PM

--- app/js/burger-menu.js
+++ app/js/burger-menu.js
@@ -1,4 +1,5 @@
 let burgerMenu = document.querySelector('.burger__menu');
+let blur = document.querySelector('.blur');
 
 const burgerMenuToggler = () => {
     burgerMenu.classList.toggle('burger__menu-shown');
