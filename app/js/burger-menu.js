9663c33c88590a293773f4946947911ca783ee53
gnlda <dunlock67@gmail.com>
2023-07-09 15:34:30 +0000

    Jul 9, 2023, 6:34 PM

--- app/js/burger-menu.js
+++ app/js/burger-menu.js
@@ -1,11 +1,26 @@
-let burgerMenu = document.querySelector('.burger__menu');
-let blur = document.querySelector('.blur');
+const burgerMenu = document.querySelector('.burger__menu');
+const blur = document.querySelector('.blur');
 const body = document.querySelector('body');
+const div = document.createElement('div');
+let countOfOpen = 0;
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+document.body.append(div);
+const scrollWidth = div.offsetWidth - div.clientWidth;
+div.remove();
+console.log(scrollWidth)
 
 const burgerMenuToggler = () => {
+    countOfOpen++;
     burgerMenu.classList.toggle('burger__menu-shown');
     blur.classList.toggle('blur-visible');
-    body.classList.toggle('unscrollable')
+    body.classList.toggle('unscrollable');
+    if (countOfOpen % 2 == 0) {
+        body.style.paddingRight = `0px`;
+    } else {
+        body.style.paddingRight = `${scrollWidth}px`;
+    }
 }
 
 
@@ -16,10 +31,12 @@ window.addEventListener("resize", () => {
     burgerMenu.classList.remove('burger__menu-shown');
     blur.classList.remove('blur-visible');
     body.classList.remove('unscrollable')
+    body.style.paddingRight = `0px`;
 })
 
 blur.addEventListener("click", () => {
     burgerMenu.classList.remove('burger__menu-shown');
     blur.classList.remove('blur-visible');
-    body.classList.remove('unscrollable')
+    body.classList.remove('unscrollable');
+    body.style.paddingRight = `0px`;
 })
